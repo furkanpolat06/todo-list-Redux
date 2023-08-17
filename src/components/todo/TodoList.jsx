b3fe2e3fdb0c36715d5bdd3d7fc5c98ec869a55c
furkanpolat06 <emnplt.0698@gmail.com>
2023-08-17 12:14:27 +0300

    fix:clear button active

--- src/components/todo/TodoList.jsx
+++ src/components/todo/TodoList.jsx
@@ -1,8 +1,10 @@
-import { useSelector } from "react-redux"
+import { useDispatch, useSelector } from "react-redux"
 import TodoItem from "./TodoItem"
+import { clearTodo } from "../../store/todoReducer"
 
 const TodoList = () => {
   const todoList=useSelector((state)=>state.todoList)
+  const dispatch=useDispatch()
 
 
 
--- src/store/todoReducer.js
+++ src/store/todoReducer.js
@@ -3,9 +3,9 @@ export const DELETE_TODO = 'DELETE_TODO'
 export const TOGGLE_TODO = 'TOGGLE_TODO'
 export const CLEAR_TODO = 'CLEAR_TODO'
 
-export const addTodo = (payload) => ({type: ADD_TODO, payload})
 // ? Payload bizim reducer veri göndereceğimiz zaman lazım olur
-
+export const addTodo = (payload) => ({type: ADD_TODO, payload})
+export const clearTodo=()=>({type:CLEAR_TODO})
 
 const initialState = {
     todoList : [
@@ -23,6 +23,15 @@ export const todoReducer= (state = initialState, { type, payload }) => {
         ],
      }
 
+     case CLEAR_TODO:
+      return initialState
+
+     case DELETE_TODO:
+      return initialState
+
+     case TOGGLE_TODO:
+      return initialState
+      
   default:
     return state
   }
